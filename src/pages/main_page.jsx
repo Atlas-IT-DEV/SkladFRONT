25749800057160ab1a4b74e608de9942fb720eb0
Николай <raspaev.niko@mail.ru>
2023-11-16 13:30:30 +0400

    Ready modal window

--- src/pages/main_page.jsx
+++ src/pages/main_page.jsx
@@ -3,9 +3,12 @@ import { VStack, useColorMode, Stack, Text, Button } from "@chakra-ui/react";
 import SideMenu from "../components/side_menu";
 import MainInfo from "../components/main_info";
 import Recent from "../components/recent";
-import EditModal from "../components/editModal/edit_modal";
+import MyModal from "../components/myModal/my_modal";
+import ProductEditForm from "../components/product_edit_form";
+import React, { useState } from "react";
 
 const MainPage = () => {
+  const [visibleModal, setVisibleModal] = useState(false);
   // const { colorMode, toggleColorMode } = useColorMode();
   return (
     <Stack direction={"row"} minH="100vh">
@@ -24,6 +27,10 @@ const MainPage = () => {
         >
           Главная страница
         </Text>
+        <Button onClick={() => setVisibleModal(true)}></Button>
+        <MyModal visibleModal={visibleModal} setVisibleModal={setVisibleModal}>
+          <ProductEditForm setVisibleModal={setVisibleModal} />
+        </MyModal>
         <MainInfo />
         <Recent />
       </VStack>
